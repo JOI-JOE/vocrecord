2f6e048696e5434ef5410b63727aa7a77b2b04ae
JOI-JOE <iknowhtml161gmail.com>
2024-12-04 08:13:48 +0700

    jhkasjfkasfj

---
+++ app/Models/ProductStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductStatusFactory> */
+    use HasFactory;
+}
